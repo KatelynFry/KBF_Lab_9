4e7cdc793c59e941ccbf91c6b1bb9b753c8f6958
KatelynFry <katelynfry@ufl.edu>
2024-04-11 12:34:13 -0400

    My added

--- encoder.py
+++ encoder.py
@@ -3,5 +3,6 @@
 def encoder(num):
     enc = ""
     for i in num:
+        # I have added the % 10 to use the remainder so that there isn't an increase in digits added to the string
         enc += str((int(i)+3) % 10)
     return enc
